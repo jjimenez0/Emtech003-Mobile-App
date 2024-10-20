29110ee3c573450c10029152f74986942bd3174d
jjimenez0 <157805479+jjimenez0@users.noreply.github.com>
2024-10-20 17:37:25 +0800

    Update Goal Setting.js

--- Goal Setting.js
+++ Goal Setting.js
@@ -1,5 +1,8 @@
-import { StyleSheet, Button, View, Text, TextInput, Pressable, Image } from 'react-native';
+import { StyleSheet, Button, View, Text, TextInput, Pressable, Image, ScrollView, FlatList } from 'react-native';
 import { useState } from 'react';
+import GoalItem from '../components/GoalItem';
+import GoalInput from '../components/GoalInput';
+import GoalHeader from '../components/GoalHeader';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -9,36 +12,22 @@ export default function App() {
     setEnteredGoalText(enteredText);
   }
 
-  function addGoalHandler() {
+  function addGoalHandler(enteredGoalText) {
     if (enteredGoalText.trim().length === 0) {
       return;
     }
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, {text: enteredGoalText, key: Math.random().toString()}]);
     setEnteredGoalText('');
   }
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textinput}
-          placeholder='Your Course Goal!'
-          value={enteredGoalText}
-          onChangeText={goalInputHandler}
-        />
-      </View>
-      <View>
-        <Pressable style={styles.button} onPress={addGoalHandler}>
-          <Text style={styles.buttonText}>Add Goals</Text>
-        </Pressable>
-      </View>
-      <View style={styles.margin}>
-        <Image source={require('@/assets/images/goal.png')} style={styles.goalImage} />
-        <Text style={styles.goalFont}>    G O A L S    </Text>
-        <Image source={require('@/assets/images/goal.png')} style={styles.goalImage} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <GoalHeader/>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal, index) => <Text key={index}>{goal}</Text>)}
+        <FlatList data={courseGoals} renderItem={({item}) => {
+          return <GoalItem text={item.text} />
+        }}/>
       </View>
     </View>
   );
@@ -54,18 +43,6 @@ const styles = StyleSheet.create({
     marginRight: 1,
     backgroundColor: '#e1e1e3',
   },
-  inputContainer: {
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  textinput: {
-    borderWidth: 3,
-    borderColor: '#8f8f8f',
-    width: '100%',
-    padding: 13,
-    borderRadius: 15,
-    backgroundColor: '#c7c5c5',
-  },
   goalsContainer: {
     flex: 3,
     borderTopWidth: 0,
@@ -74,41 +51,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 3,
     borderColor: '#8f8f8f',
     paddingTop: 10,
-    paddingLeft: 20,
+    paddingLeft: 10,
     marginBottom: 10,
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 5,
-    elevation: 3,
-    backgroundColor: 'black',
-  },
-  buttonText: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  },
-  margin: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 3,
-    marginTop: 10,
-    paddingTop: 10,
-    paddingBottom: 10,
-    borderColor: '#8f8f8f',
-    backgroundColor: '#c7c5c5',
-  },
-  goalFont: {
-    fontSize: 15,
-  },
-  goalImage: {
-    height: 30,
-    width: 30,
   },
 });
